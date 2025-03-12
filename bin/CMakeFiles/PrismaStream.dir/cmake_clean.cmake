4e231b8d88a23d8db67735a0d8638c3b9e93d3d7
tailsmonster <aroca.twelve@proton.me>
2025-03-11 23:03:01 -0400

    build

---
+++ bin/CMakeFiles/PrismaStream.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PrismaStream.dir/link.d"
+  "CMakeFiles/PrismaStream.dir/src/main.cpp.o"
+  "CMakeFiles/PrismaStream.dir/src/main.cpp.o.d"
+  "PrismaStream"
+  "PrismaStream.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/PrismaStream.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
