5bbeaa09d5928237ec562a85f88b0eba9920fd2e
tailsmonster <aroca.twelve@proton.me>
2025-06-04 17:11:27 -0400

    updated init header

--- include/Init.hpp
+++ include/Init.hpp
@@ -2,6 +2,18 @@
 
 #include <iostream>
 #include <portaudio.h>
+#include "Metadata.h"
 
 // init PortAudio and get the default output device
-PaDeviceIndex initAudio();
+
+namespace App::Init {
+  void initPortAudio();
+
+  int getDeviceCount();
+  
+  void printAllDevicesInfo(int numDevices);
+  
+  PaDeviceIndex getDefaultIndex();
+  
+  const PaDeviceInfo* printDeviceInfo(PaDeviceIndex idx);
+}
